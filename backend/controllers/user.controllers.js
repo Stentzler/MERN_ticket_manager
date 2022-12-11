11b7cf19d79c983d64349956d7ee7c0a63649ff4
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-11 01:06:47 -0300

    feat: create user working well with DB

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.model');
 
 // @desc register new user
 // @route /api/users
@@ -10,6 +12,29 @@ const registerUser = asyncHandler(async (req, res) => {
 		res.status(400);
 		throw new Error('All fields required');
 	}
+
+	const userExists = await User.findOne({email});
+	if (userExists) {
+		res.status(400);
+		throw new Error('Email already registered');
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const user = await User.create({
+		name,
+		email,
+		password: hashedPassword,
+	});
+
+	if (user) {
+		res.status(201).send({_id: user._id, name: user.name, email: user.email});
+	} else {
+		res.status(400);
+		throw new error('Invalid User Data');
+	}
+
 	res.send('Resgister Route');
 });
 

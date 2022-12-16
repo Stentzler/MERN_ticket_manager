fddc9d763bfc404512e8138601cbe08ce6468225
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-16 20:13:03 -0300

    feat: ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
@@ -16,12 +17,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) =>
-	res.status(200).send({message: 'Welcome to the Ticket-app'})
-);
-
 // Routes
 app.use('/api/tickets', require('./routes/ticket.routes'));
 app.use('/api/users', require('./routes/user.routes'));
+
+// Production ENV
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) =>
+		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
+	);
+} else {
+	app.get('/', (req, res) =>
+		res.status(200).send({message: 'Welcome to the Ticket-app'})
+	);
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

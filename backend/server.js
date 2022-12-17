4e90c19f72ad83b0d3c27413a883f1c80df3a5a6
Vinicius Stentzler <vin.stentzler@gmail.com>
2022-12-16 22:58:06 -0300

    Added API endpoint for requests

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.get('/', (req, res) =>
+	res.status(200).send({message: 'Welcome to the Ticket-app'})
+);
+
 app.use(cors());
 
 app.use(express.json());
@@ -20,18 +24,5 @@ app.use(express.urlencoded({extended: false}));
 // Routes
 app.use('/api/tickets', require('./routes/ticket.routes'));
 app.use('/api/users', require('./routes/user.routes'));
-
-// Production ENV
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-	app.get('*', (req, res) =>
-		res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html')
-	);
-} else {
-	app.get('/', (req, res) =>
-		res.status(200).send({message: 'Welcome to the Ticket-app'})
-	);
-}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
